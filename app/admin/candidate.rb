4854fb41875a8304f0180bec239a0d9f6c9802a0
Marco Marignoli <contact@marcomarignoli.it>
2015-11-06 12:36:15 +0000

    Fixed active admin candidate form

--- app/admin/candidate.rb
+++ app/admin/candidate.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Candidate do
-
+  form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :alias
+      f.input :date_of_birth, :as => :datetime_picker
+      f.input :gender, :label => 'Gender', :as => :select, :collection => ["Male", "Female", "Other"]
+      f.input :photo_url, as: :string
+      f.input :party_profile_url, as: :string
+      f.input :website_url, as: :string
+      f.input :twitter_url, as: :string
+      f.input :facebook_url, as: :string
+      f.input :instagram_url, as: :string
+      f.input :linkedin_url, as: :string
+      f.input :youtube_url, as: :string
+      f.input :source_url, as: :string
+      f.input :voting_record_url, as: :string
+      f.input :claimed_expenses, :step => 'any'
+    end
+    f.actions
+  end
 end
