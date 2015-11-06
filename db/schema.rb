0fcf70649eea0511e0f311346a79f3be96d9e36e
Tony Byrne <tonyb@byrnehq.com>
2015-11-06 12:19:25 +0000

    Add foreign key columns for party and constituency to candidates table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106112620) do
+ActiveRecord::Schema.define(version: 20151106121528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20151106112620) do
     t.decimal  "claimed_expenses",  precision: 9, scale: 2
     t.datetime "created_at",                                                null: false
     t.datetime "updated_at",                                                null: false
+    t.integer  "party_id"
+    t.integer  "constituency_id"
   end
 
   create_table "constituencies", force: :cascade do |t|
