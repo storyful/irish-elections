efe54a1d9ccaa75b165caffc1e746b852dfa2edb
Tony Byrne <tonyb@byrnehq.com>
2015-11-06 14:28:18 +0000

    Basic part page

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,7 +2,10 @@ class PartiesController < ApplicationController
   api :GET, '/parties'
   def index
     respond_to do |format|
-      format.json { render_for_api :default, json: Party.all }
+      @parties = Party.all
+      format.html
+      format.json { render_for_api :default, xml: @parties }
+      format.xml  { render_for_api :default, xml: @parties }
     end
   end
 end
