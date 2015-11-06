4cb9990e771c2acb0f57b96bf506396f40efe304
Gianni Leggio <giannileggio@gmail.com>
2015-11-06 13:04:05 +0000

    Test on parties request fixed

--- spec/requests/parties_rspec.rb
+++ spec/requests/parties_rspec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Parties', type: :request do
-  subject { get '/parties' }
+  subject { get '/parties.json' }
 
   it 'gets the list of parties' do
     create(:party)
     subject
-    expect(JSON.parse(response.body).first).to include_json(name: 'Dictators' )
+    expect(JSON.parse(response.body).first).to include_json(name: 'Dictators')
   end
 end
