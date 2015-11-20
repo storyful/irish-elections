6b98215cb91481a74810d889e885c6ab26d2f093
Gianni Leggio <giannileggio@gmail.com>
2015-11-20 12:45:09 +0000

    Search field in home page

--- db/schema.rb
+++ db/schema.rb
@@ -72,18 +72,18 @@ ActiveRecord::Schema.define(version: 20151120113405) do
     t.integer  "party_id"
     t.integer  "constituency_id"
     t.string   "email"
+    t.string   "slug"
     t.boolean  "current_councillor",                         default: false, null: false
     t.boolean  "current_senator",                            default: false, null: false
     t.text     "snapchat_url"
     t.string   "phone_1"
     t.string   "phone_2"
-    t.string   "slug"
   end
 
   create_table "constituencies", force: :cascade do |t|
-    t.string  "name",      null: false
+    t.string  "name",                  null: false
     t.string  "slug"
-    t.integer "num_seats", null: false
+    t.integer "num_seats", default: 0, null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20151120113405) do
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "parties", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "slug"
   end
 
